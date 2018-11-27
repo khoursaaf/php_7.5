24e35b98cd37f49637c71cb51d84badfc6145107
khoursaaf <khoursa@gmail.com>
2018-11-27 15:22:24 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    <select name="gender">
+      <option value="Mr">Mr</option>
+      <option value="Mme">Mme</option>
+      <option value="Autres">Autres</option>
+    </select>
+    <p><input type="text" name="lastname" placeholder="Votre nom"></p>
+    <p><input type="text" name="firstname" placeholder="Votre Prenom"></p>
+    <p><input type="submit" value="Valider">
+    </p>
+  </form>
+</body>
+</html>
